486a324ef4e0e24c64d1ecdbd24a3eab7869e374
astroupia <103548612+astroupia@users.noreply.github.com>
2025-07-04 11:03:07 +0300

    chore: cleaning up unwanted modules and files

--- apps/api/src/n8n/n8n.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('n8n')
-export class N8nController {}
--- apps/api/src/smartlead/smartlead.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SmartleadController } from './smartlead.controller';
-import { SmartleadService } from './smartlead.service';
-
-@Module({
-  controllers: [SmartleadController],
-  providers: [SmartleadService]
-})
-export class SmartleadModule {}
