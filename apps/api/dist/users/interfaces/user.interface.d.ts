caf290df0bcb3ae11341228ddcdf30c9a02c88d0
Michael seyoum <104850692+itskarloos@users.noreply.github.com>
2025-07-04 11:52:15 +0300

    chore: changes on config file

--- apps/api/dist/users/interfaces/user.interface.d.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-    id: string;
-    email: string;
-    name?: string;
-    role: 'ADMIN' | 'MEMBER' | 'READ_ONLY';
-    companyId: string;
-    createdAt: Date;
-    updatedAt: Date;
-}
